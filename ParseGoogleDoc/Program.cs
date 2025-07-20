68b466bbdbaec6443ad4b527b31e4ad91ab57187
Hongsup Shim <iamhongsup@gmail.com>
2025-07-21 06:48:43 +0900

    Add getting Google doc

--- ParseGoogleDoc/Program.cs
+++ ParseGoogleDoc/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http;
+using System.Threading.Tasks;
 
 Console.WriteLine("Hello, World!");
+
+
+async Task<string> GetGoogleDocText(string docId)
+{
+    var url = $"https://docs.google.com/document/d/{docId}/export?format=txt";
+    using (var client = new HttpClient())
+    {
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
